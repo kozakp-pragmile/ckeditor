a6d175886bb786eafd5847af5d4d183010d41050
Piotr Kozak <piotr.kozak@pragmile.com>
2025-08-27 09:25:01 +0200

    Add CKEditor inspector

--- block-widget/main.js
+++ block-widget/main.js
@@ -1,28 +1,30 @@
 // main.js
 
 import {
-	ClassicEditor,
-	Bold,
-	Italic,
-	Essentials,
-	Heading,
-	List,
-	Paragraph
+  ClassicEditor,
+  Bold,
+  Italic,
+  Essentials,
+  Heading,
+  List,
+  Paragraph
 } from 'ckeditor5';
 import 'ckeditor5/ckeditor5.css';
 import SimpleBox from "./simplebox/simplebox.js";
+import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
 
 ClassicEditor
-	.create( document.querySelector( '#editor' ), {
-		plugins: [ Essentials, Paragraph, Heading, List, Bold, Italic, SimpleBox ],
-		toolbar: [ 'heading', 'bold', 'italic', 'numberedList', 'bulletedList' ],
-		licenseKey: 'GPL'
-	} )
-	.then( editor => {
-		console.log( 'Editor was initialized', editor );
-		// Expose for playing in the console.
-		window.editor = editor;
-	} )
-	.catch( error => {
-		console.error( error.stack );
-	} );
+  .create(document.querySelector('#editor'), {
+    plugins: [Essentials, Paragraph, Heading, List, Bold, Italic, SimpleBox],
+    toolbar: ['heading', 'bold', 'italic', 'numberedList', 'bulletedList'],
+    licenseKey: 'GPL'
+  })
+  .then(editor => {
+    console.log('Editor was initialized', editor);
+    CKEditorInspector.attach({'editor': editor});
+    // Expose for playing in the console.
+    window.editor = editor;
+  })
+  .catch(error => {
+    console.error(error.stack);
+  });
