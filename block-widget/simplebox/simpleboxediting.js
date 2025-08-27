54531bf53c48d8e929debb5185f36198acb5be17
Piotr Kozak <piotr.kozak@pragmile.com>
2025-08-27 09:13:55 +0200

    Define the schema

--- block-widget/simplebox/simpleboxediting.js
+++ block-widget/simplebox/simpleboxediting.js
@@ -1,7 +1,39 @@
-import { Plugin } from 'ckeditor5';
+import {Plugin} from 'ckeditor5';
 
 export default class SimpleBoxEditing extends Plugin {
   init() {
-    console.log( 'SimpleBoxEditing#init() got called' );
+    console.log('SimpleBoxEditing#init() got called');
+
+    this._defineSchema();
+  }
+
+  _defineSchema() {
+    const schema = this.editor.model.schema;
+
+    schema.register('simpleBox', {
+      // Behaves like a self-contained block object (e.g. a block image)
+      // allowed in places where other blocks are allowed (e.g. directly in the root).
+      inheritAllFrom: '$blockObject'
+    });
+
+    schema.register('simpleBoxTitle', {
+      // Cannot be split or left by the caret.
+      isLimit: true,
+
+      allowIn: 'simpleBox',
+
+      // Allow content which is allowed in blocks (i.e. text with attributes).
+      allowContentOf: '$block'
+    });
+
+    schema.register('simpleBoxDescription', {
+      // Cannot be split or left by the caret.
+      isLimit: true,
+
+      allowIn: 'simpleBox',
+
+      // Allow content which is allowed in the root (e.g. paragraphs).
+      allowContentOf: '$root'
+    });
   }
 }
